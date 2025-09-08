390e6e24fbc0ebbbb3ae8d24c1fc69654439e6e6
Andris Spalvis <spalvis.andris@gmail.com>
2025-09-08 14:59:21 +0300

    .gitignore

--- appsmith.net/hosting/appsmith.net.AppHost/AppHost.cs
+++ appsmith.net/hosting/appsmith.net.AppHost/AppHost.cs
@@ -1,13 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var volumesDir = $"{AppHostExtensions.GetRepoRootDir()}/volumes";
+
 var username = builder.AddParameter("username", "postgres");
 var password = builder.AddParameter("password", "postgres");
 
 var postgres = builder.AddPostgres("postgres", username, password, port: 5432)
-    .WithOtlpExporter();
+    .WithOtlpExporter()
+    .WithBindMount($"{volumesDir}/postgres/data", "/var/lib/postgresql/data");
 
 var appSmith = builder.AddContainer("appsmith", "index.docker.io/appsmith/appsmith-ee")
-    .WithBindMount($"{AppHostExtensions.GetRepoRootDir()}/volumes/appsmith/appsmith-stacks", "/appsmith-stacks")
+    .WithBindMount($"{volumesDir}/appsmith/appsmith-stacks", "/appsmith-stacks")
     .WithEndpoint(4200, 80, scheme: "http")
     .WithEndpoint(4201, 443, scheme: "https");
 
